302c6043bae7502fb330f403d1f3c55f7521b635
Clark Carter <clark@clarkcarter.com>
2017-09-20 19:56:20 +1000

    read from Firebase

--- src/TaskItem.js
+++ src/TaskItem.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { List } from 'semantic-ui-react';
 
 class TaskItem extends React.Component {
   render() {
     return (
-      <List.Item>
-        <List.Content verticalAlign="middle">
-          Task Item
-        </List.Content>
-      </List.Item>
+      <div>
+        {this.props.task}
+      </div>
     )
   }
 }
--- src/TaskList.js
+++ src/TaskList.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import _ from 'lodash';
 import TaskItem from './TaskItem';
 
 class TaskList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: []
+    };
+    //defines a listener that keeps an eye of the database of tasks and when a new task is added to the database, it calls the getTasks function with the database of tasks as it's argument
+    const taskList = this.props.db.database().ref('tasks');
+    taskList.on('value', snapshot => {
+      this.getTasks(snapshot.val());
+    });
+  }
+
+  getTasks(value) {
+    let taskVal = value;
+    let tasks = _(taskVal).keys().map(taskKey => {
+        let cloned = _.clone(taskVal[taskKey]);
+        cloned.key = taskKey;
+        return cloned;
+    }).value();
+    this.setState({
+      tasks: tasks
+    });
+  }
+
   render() {
+    const tasks = this.state.tasks.map((task) => {
+      return (
+        <li key={task.key}>
+          <TaskItem task={task.task}/>
+        </li>
+      )
+    });
     return (
-      <div>
-        <TaskItem />
-      </div>
+      <ul>
+        {tasks}
+      </ul>
     )
   }
 }
